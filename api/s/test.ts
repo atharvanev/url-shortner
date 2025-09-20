e26138741ffe3f6fd2193e3484f215ac8ceb5390
Atharva <147217989+atharvanev@users.noreply.github.com>
2025-09-20 01:47:37 -0700

    troubleshooting in prod

---
+++ api/s/test.ts
@@ -0,0 +1,5 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
+    res.status(200).json({ message: 'Static route in s/ works!' });
+}
